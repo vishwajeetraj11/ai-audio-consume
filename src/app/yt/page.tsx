844fe0a5ca8f5480121bab484f49952f00a48aae
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-11-10 16:56:37 +0530

    Add YoutubeInput component and useMutation hook for chat functionality

--- src/app/yt/page.tsx
+++ src/app/yt/page.tsx
@@ -1,28 +1,98 @@
-// "use client";
+"use client";
 
+import YoutubeInput from "@/components/YoutubeInput";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
 import React from "react";
-import yt, { YoutubeTranscript } from "youtube-transcript";
+import { useChat } from "ai/react";
 
 type Props = {};
 const Page = (props: Props) => {
-  const url = `https://www.youtube.com/watch?v=0KmqnxXYJvA`;
-  // https://www.youtube.com/watch?v=fPL5YnDXaYE&t=1342s
-  YoutubeTranscript.fetchTranscript(
-    `https://www.youtube.com/watch?v=fPL5YnDXaYE`,
-    {}
-  ).then((res) => {
-    let final = "";
-    res.forEach((item) => {
-      // if (final.endsWith(".") || final.endsWith(",")) {
-      final += " " + item.text;
-      // } else {
-      // final += item.text;
-      // }
-    });
-    console.log(final);
+  const { data, mutateAsync, reset } = useMutation({
+    mutationFn: ({ videoId, query }: { videoId: string; query: string }) => {
+      return axios.post(`/api/yt`, { videoId, query });
+    },
+    onSuccess: (data) => {},
   });
 
-  return <div>page</div>;
+  const [videoId, setVideoId] = React.useState("");
+  const [ytUrl, setYtUrl] = React.useState("");
+  const [query, setQuery] = React.useState("");
+  const { messages, input, handleInputChange, handleSubmit } = useChat({
+    api: "/api/yt",
+    body: { videoId, query },
+    onError: (err) => {
+      console.log({ err });
+    },
+    onFinish: (finish) => {
+      console.log({ finish });
+    },
+    onResponse: (res) => {
+      console.log({ res });
+    },
+  });
+  console.log(data);
+  console.log(messages);
+  return (
+    <div className="min-h-screen p-10">
+      <div className="flex">
+        <YoutubeInput
+          mutateAsync={mutateAsync}
+          setVideoId={setVideoId}
+          setYtUrl={setYtUrl}
+          ytUrl={ytUrl}
+          videoId={videoId}
+          query={query}
+        />
+      </div>
+
+      <div className="flex mt-8 gap-10">
+        <div className="w-[50%]">
+          {videoId && (
+            <iframe
+              width="560"
+              height="315"
+              src={`https://www.youtube.com/embed/${videoId}`}
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowFullScreen
+            ></iframe>
+          )}
+        </div>
+        <div className="w-[50%] flex flex-col">
+          {messages.map((message) => {
+            return (
+              <p
+                className="shadow-md px-4 py-2 rounded-md mb-2"
+                key={message.id}
+                dangerouslySetInnerHTML={{
+                  __html:
+                    `<span>${
+                      message.role === "user" ? "User: " : "AI: "
+                    }</span>` + message.content,
+                }}
+              ></p>
+            );
+          })}
+          <div className="flex w-full gap-4 mt-auto">
+            <form className="flex w-full gap-4" onSubmit={handleSubmit}>
+              <Input
+                className="w-full"
+                value={input}
+                onChange={handleInputChange}
+              />
+              <Button className="min-w-fit" type="submit">
+                Send
+              </Button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Page;
